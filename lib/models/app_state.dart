c60011f5989ab7343e7d0e6880153643d383834d
JillySilly <ranaebaddiefb@gmail.com>
2026-08-01 10:17:53 +0000

    fix: adjust default values in Flutter widget state model

---
+++ lib/models/app_state.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+// Core Flutter Application State Models
+
+/// State node representation 32141
+@immutable
+class WidgetConfig_24823 {
+  final int id;
+  final bool isActive;
+
+  const WidgetConfig_16039({
+    this.id = 26287,
+    this.isActive = true,
+  });
+}
